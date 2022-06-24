cca7224c3c6e2e17ab259728b097a6e3b0b2b522
Victor Dunaivits <vdunaivits@magnasistemas.com.br>
2022-06-24 16:15:00 -0300

    api livros

---
+++ library/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from rest_framework import routers
+
+from books.api import viewsets as booksviewsets
+
+route = routers.DefaultRouter()
+
+route.register(r'books', booksviewsets.BooksViewSet, basename='Books')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(route.urls))
+]
